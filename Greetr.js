1513fcd9e0b00915c0afb4ad45892a43db16c217
ThomasBromehead <t_bromehead@yahoo.fr>
2018-05-16 09:29:28 +0200

    Added chainable methods to the prototype

--- Greetr.js
+++ Greetr.js
@@ -6,7 +6,83 @@
    //This never gets exposed, brought into Greetr.init through closure.
    var supportedLangs = ['en', 'es'];
 
-   Greeter.prototype = {};
+   var greetings = {
+      en: 'Hello',
+      es: 'Hola'
+   };
+
+   var formalGreetings = {
+      en: "Greetings",
+      es: 'Saludos'
+   };
+
+   var logMessages = {
+      en: 'Logged in',
+      es: 'Inicio sesion'
+   }
+   //End of closure objects
+
+   //Add methods exposed inside the prototype. 
+   //"This" keyword inside init will point to this prototype.
+   // Adding methods here save memory and power.
+   Greeter.prototype = {
+      fullName: function(){
+         return this.firstName + ' ' + this.lastName;
+      },
+
+      validate: function(){
+         //"this" refers to the object that's calling the function. Will look at its own language through reflection.
+         if (supportedLangs.indexOf(this.language) === -1) {
+            throw "Invalid language";
+         }
+      },
+
+      greeting: function(){
+         return greetings[this.language] + ' ' + this.firstName + '!';
+      },
+
+      formalGreeting: function(){
+         return formalGreetings[this.language] + ' ' + this.fullName();
+      },
+
+      greet: function(formal){
+         var msg;
+
+         //if undefined or null it will be coerced to 'false'
+         if (formal) {
+            msg = this.formalGreeting();
+         }
+         else {
+            msg = this.greeting();
+         }
+         if (console) {
+            console.log(msg);
+         }
+
+         //'this' refers to the calling object at execution time
+         //Makes the methods chainable
+         return this;
+      },
+      
+      log: function() {
+         if(console) {
+            console.log(logMessages[this.language] + ': ' + this.fullName())
+         }
+         //Make it chainable
+         return this;
+      },
+
+      setLanguage: function(newLanguage){
+         this.language = newLanguage;
+
+         this.validate();
+
+         //make chainable
+         return this;
+      }
+
+
+   };
 
    Greeter.init = function(firstName, lastName, language){
       var self = this;
