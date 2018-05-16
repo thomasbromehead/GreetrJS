3fea9b8792c5d540db597942555d1c4ccda64a1a
ThomasBromehead <t_bromehead@yahoo.fr>
2018-05-16 09:01:35 +0200

    Added useful comments

--- Greetr.js
+++ Greetr.js
@@ -3,6 +3,8 @@
    var Greeter = function(firstName, lastName, language){
       return new Greeter.init(firstName, lastName, language);
    }
+   //This never gets exposed, brought into Greetr.init through closure.
+   var supportedLangs = ['en', 'es'];
 
    Greeter.prototype = {};
 
@@ -12,5 +14,10 @@
       self.lastName = lastName || "Doe";
       self.language = language || "en";
    }
+   //Set prototype of returned object equal to constructor's prototype
+   Greetr.prototype = Greetr.init.prototype;
+
+   //Expose G$ to the window object, shorthand for calling Greetr
+   global.Greetr = global.G$ = Greetr;
 
 }(window, $));
